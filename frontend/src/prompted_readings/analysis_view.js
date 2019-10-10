5acd87382d7ae5795288c350fd1ec970c1de5be0
Amy <leiamy12@gmail.com>
2019-10-10 16:07:55 -0400

    Updated front end to reflect our function's findings

--- frontend/src/prompted_readings/analysis_view.js
+++ frontend/src/prompted_readings/analysis_view.js
@@ -24,18 +24,29 @@ class AnalysisView extends React.Component {
             console.log(e);
         }
     }
+    //
+    /**
+     * Create paragraph tags for the question and revisit count
+     */
+    // let revisits = median_revisits.map((medians) =>
+    //     <p>{medians}</p>
+    // );
 
     render() {
         if (this.state.analysis !== null) {
             const {  // object destructuring:
                 total_view_time,
+                median_revisits,
             } = this.state.analysis;
-
             return (
                 <div>
                     <h1>Analysis of Student Responses</h1>
                     <h3>Total view time</h3>
                     <p>{total_view_time} seconds</p>
+                    <h3>Median times a question was revisited</h3>
+                    { Object.keys(median_revisits).map(function(median, i){
+                        return <p key={i}> {median} : {median_revisits[median]} </p>;
+                    })}
                 </div>
             );
         } else {
